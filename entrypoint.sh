9cc8e20e7f5fd1d237da5bf3dddd698d1a890802
Sharad Raj <dev_sharad@outlook.com>
2021-11-13 18:26:19 +0000

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "========================= Changing directory to $1 ========================="
 cd $1
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
-if [ $2 = "true" ]; then
+if [ $2 = true ]; then
   echo "========================= Doing Signing ========================="
   ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation
 else
