3d7de28fb676949a82988883e7362fd9d468c95f
Sharad Raj <dev_sharad@outlook.com>
2021-11-13 16:58:52 +0000

    Added conditions for signing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 echo "========================= Changing directory to $1 ========================="
 cd $1
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
-( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation --skipSigning
+if [ $2 = "true" ]; then
+  ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation
+else
+  ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation --skipSigning
+fi
 cp ./*.apk ..
 echo "========================= APK building finished ========================="
