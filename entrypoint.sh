e8cbc764962700a514e0ddb6a3552eca32a7a0bb
Sharad Raj <dev_sharad@outlook.com>
2021-11-13 17:54:45 +0000

    Add echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ echo "========================= Changing directory to $1 =======================
 cd $1
 echo "========================= Building APK, it may take 3-4 minutes or more ========================="
 if [ $2 = "true" ]; then
+  echo "========================= Doing Signing ========================="
   ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation
 else
+  echo "========================= Skipping Signing ========================="
   ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | bubblewrap build --skipPwaValidation --skipSigning
 fi
 cp ./*.apk ..
